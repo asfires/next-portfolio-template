35b4035c7f12bbdd6e3e00414a7d9cfe0e4492f5
Adam Firestone <asfirestone1@gmail.com>
2023-09-02 20:04:52 -0500

    feat:add mobile nav to site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,15 +4,17 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
+import { MobileNav } from "@/components/mobile-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center justify-between sm:space-x-0">
+        <MainNav items={siteConfig.nav} />
+        <MobileNav items={siteConfig.nav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
+          <nav className="flex items-center space-x-1 ">
             <Link
               href={siteConfig.links.github}
               target="_blank"
@@ -29,7 +31,7 @@ export function SiteHeader() {
               </div>
             </Link>
             <Link
-              href={siteConfig.links.twitter}
+              href={siteConfig.links.linkedin}
               target="_blank"
               rel="noreferrer"
             >
@@ -39,8 +41,8 @@ export function SiteHeader() {
                   variant: "ghost",
                 })}
               >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
+                <Icons.linkedin className="h-5 w-5 fill-current" />
+                <span className="sr-only">LinkedIn</span>
               </div>
             </Link>
             <ThemeToggle />
