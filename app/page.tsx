90ece3ef29d24e71f54baf51cdcb00fa4ca90b10
Adam Firestone <asfirestone1@gmail.com>
2023-09-02 19:57:52 -0500

    feat:root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
@@ -5,35 +6,45 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+    <>
+      <div className="flex h-[calc(100vh-164px)] items-center justify-center overflow-auto">
+        <div className="max-w-[260px] grid-cols-1 space-y-4 md:max-w-[487px]">
+          <div className="relative m-auto flex h-[200px] w-[200px] justify-center md:h-[240px] md:w-[240px]">
+            <Image
+              src={siteConfig.images.home.src}
+              alt={siteConfig.images.home.alt}
+              fill
+              priority
+              sizes="(max-width: 240px) 100vw"
+              className="rounded-full object-cover pb-2"
+            />
+          </div>
+          <h1 className="flex justify-center text-6xl font-extrabold tracking-tighter md:text-8xl">
+            Hello! 👋
+          </h1>
+          <h2 className="text-center text-muted-foreground md:text-lg">
+            I like to make full-stack apps with Next.js and TypeScript.
+          </h2>
+          <div className="flex justify-center gap-4">
+            <Link
+              href={siteConfig.files.resume}
+              target="_blank"
+              rel="noreferrer"
+              className={buttonVariants()}
+            >
+              Resume
+            </Link>
+            <Link
+              href={siteConfig.links.email}
+              className={buttonVariants({
+                variant: "outline",
+              })}
+            >
+              Reach out
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    </>
   )
 }
